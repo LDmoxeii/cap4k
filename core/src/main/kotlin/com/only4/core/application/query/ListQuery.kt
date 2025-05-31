40d37b8bfcfd39e3084eff53074e3d54ae91ec2f
LD_moxeii <15976781430@163.com>
2025-05-31 21:48:34 +0800

    refactor(core): 优化查询模块接口定义和文档注释
    - 为 ListQuery、ListQueryParam、PageQuery、PageQueryParam 和 Query接口添加了更详细的文档注释
    - 明确了各接口的用途、参数类型和返回值类型
    - 优化了代码结构，提高了代码的可读性和可维护性

--- core/src/main/kotlin/com/only4/core/application/query/ListQuery.kt
+++ core/src/main/kotlin/com/only4/core/application/query/ListQuery.kt
@@ -4,9 +4,13 @@ import com.only4.core.application.RequestParam
 
 /**
  * 列表查询接口
+ * 用于处理返回列表类型结果的查询操作，支持自定义查询参数
  *
  * @author binking338
- * @date
+ * @date 2024/9/6
+ *
+ * @param ITEM 列表项类型，表示查询结果中的单个数据项
+ * @param PARAM 查询参数类型，必须实现RequestParam接口，用于定义查询条件
  */
 interface ListQuery<ITEM : Any, PARAM : RequestParam<List<ITEM>>> :
     Query<List<ITEM>, PARAM>
--- core/src/main/kotlin/com/only4/core/application/query/PageQuery.kt
+++ core/src/main/kotlin/com/only4/core/application/query/PageQuery.kt
@@ -4,10 +4,14 @@ import com.only4.core.application.RequestParam
 import com.only4.core.share.PageData
 
 /**
- * 分页查询
+ * 分页查询接口
+ * 用于处理返回分页数据的查询操作
  *
  * @author binking338
- * @date
+ * @date 2024/9/6
+ *
+ * @param ITEM 分页数据项类型
+ * @param PARAM 查询参数类型，必须实现RequestParam接口
  */
 interface PageQuery<ITEM : Any, PARAM : RequestParam<PageData<ITEM>>> :
     Query<PageData<ITEM>, PARAM>
