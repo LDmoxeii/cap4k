5fc786e45dd220cacc13f577597b2e333df8af3c
LD_moxeii <15976781430@163.com>
2025-05-31 21:45:57 +0800

    refactor(core): 重构集成事件监督者
    - 重新定义 IntegrationEventSupervisor 接口，增加事件生命周期管理相关方法
    - 实现 IntegrationEventSupervisorSupport 类，提供事件生命周期管理功能
    -优化事件附加、解除附加、发布和持久化的逻辑
    - 重构线程本地变量的使用，提高代码可读性和性能

--- core/src/main/kotlin/com/only4/core/application/event/IntegrationEventSupervisor.kt
+++ core/src/main/kotlin/com/only4/core/application/event/IntegrationEventSupervisor.kt
@@ -10,18 +10,21 @@ import java.time.Duration
 import java.time.LocalDateTime
 
 /**
- * 集成事件控制器
+ * 集成事件监督者接口
+ * 负责管理和控制集成事件的生命周期，包括事件的附加、解除附加和发布
  *
  * @author binking338
  * @date 2024/8/25
  */
 interface IntegrationEventSupervisor {
-
     /**
      * 附加事件到持久化上下文
+     * 将事件添加到当前线程的上下文中，等待事务提交后发布
      *
      * @param eventPayload 事件消息体
-     * @param schedule     指定时间发送
+     * @param schedule 指定时间发送
+     * @param delay 延迟时间
+     * @throws IllegalArgumentException 如果事件类型不是集成事件
      */
     fun <EVENT : Any> attach(
         eventPayload: EVENT,
@@ -30,7 +33,8 @@ interface IntegrationEventSupervisor {
     )
 
     /**
-     * 从持久化上下文剥离事件
+     * 从持久化上下文解除事件
+     * 将事件从当前线程的上下文中移除
      *
      * @param eventPayload 事件消息体
      */
@@ -38,8 +42,11 @@ interface IntegrationEventSupervisor {
 
     /**
      * 发布指定集成事件
+     * 立即发布事件，不等待事务提交
+     *
      * @param eventPayload 集成事件负载
-     * @param schedule     指定时间发送
+     * @param schedule 指定时间发送
+     * @param delay 延迟时间
      */
     fun <EVENT : Any> publish(
         eventPayload: EVENT,
@@ -48,16 +55,23 @@ interface IntegrationEventSupervisor {
     )
 
     companion object {
+        /**
+         * 获取集成事件监督者实例
+         */
         val instance: IntegrationEventSupervisor
             get() = IntegrationEventSupervisorSupport.instance
 
+        /**
+         * 获取集成事件管理器实例
+         */
         val manager: IntegrationEventManager
             get() = IntegrationEventSupervisorSupport.manager
     }
 }
 
 /**
- * 默认事件管理器
+ * 默认集成事件监督者实现
+ * 提供集成事件的完整生命周期管理，包括事件的附加、解除附加、持久化和发布
  *
  * @author binking338
  * @date 2024/8/28
@@ -70,101 +84,84 @@ open class DefaultIntegrationEventSupervisor(
     private val svcName: String,
 ) : IntegrationEventSupervisor,
     IntegrationEventManager {
+
     override fun <EVENT : Any> attach(eventPayload: EVENT, schedule: LocalDateTime, delay: Duration) {
-        require(!eventPayload.javaClass.isAnnotationPresent(IntegrationEvent::class.java)) { "事件类型必须为集成事件" }
+        require(eventPayload.javaClass.isAnnotationPresent(IntegrationEvent::class.java)) { "事件类型必须为集成事件" }
+
         val eventPayloads = TL_EVENT_PAYLOADS.get() ?: HashSet<Any>().also { TL_EVENT_PAYLOADS.set(it) }
         eventPayloads.add(eventPayload)
         putDeliverTime(eventPayload, schedule)
-        integrationEventInterceptorManager.orderedIntegrationEventInterceptors.forEach {
-            it.onAttach(
-                eventPayload,
-                schedule
-            )
+
+        integrationEventInterceptorManager.orderedIntegrationEventInterceptors.forEach { interceptor ->
+            interceptor.onAttach(eventPayload, schedule)
         }
     }
 
-
     override fun <EVENT : Any> detach(eventPayload: EVENT) {
-        val eventPayloads = TL_EVENT_PAYLOADS.get() ?: HashSet<Any>().also { TL_EVENT_PAYLOADS.set(it) }
-        eventPayloads.remove(eventPayload)
-        integrationEventInterceptorManager.orderedIntegrationEventInterceptors.forEach {
-            it.onDetach(
-                eventPayload
-            )
+        TL_EVENT_PAYLOADS.get()?.let { eventPayloads ->
+            eventPayloads.remove(eventPayload)
+            integrationEventInterceptorManager.orderedIntegrationEventInterceptors.forEach { interceptor ->
+                interceptor.onDetach(eventPayload)
+            }
         }
     }
 
     override fun release() {
-        val eventPayloads = mutableSetOf(this.popEvents())
-        val persistedEvents: MutableList<EventRecord> = ArrayList(eventPayloads.size)
+        val eventPayloads = mutableSetOf(popEvents())
+        val persistedEvents = ArrayList<EventRecord>(eventPayloads.size)
+
         eventPayloads.forEach { eventPayload ->
-            val deliverTime = this.getDeliverTime(eventPayload)
-            val event = eventRecordRepository.create()
-            event.init(
-                eventPayload,
-                this.svcName,
-                deliverTime,
-                Duration.ofMinutes(DEFAULT_EVENT_EXPIRE_MINUTES),
-                DEFAULT_EVENT_RETRY_TIMES
-            )
-            event.markPersist(true)
-            integrationEventInterceptorManager.orderedEventInterceptors4IntegrationEvent.forEach {
-                it.prePersist(event)
+            val deliverTime = getDeliverTime(eventPayload)
+            val event = eventRecordRepository.create().apply {
+                init(
+                    eventPayload,
+                    svcName,
+                    deliverTime,
+                    Duration.ofMinutes(DEFAULT_EVENT_EXPIRE_MINUTES),
+                    DEFAULT_EVENT_RETRY_TIMES
+                )
+                markPersist(true)
             }
+
+            integrationEventInterceptorManager.orderedEventInterceptors4IntegrationEvent.forEach { it.prePersist(event) }
             eventRecordRepository.save(event)
-            integrationEventInterceptorManager.orderedEventInterceptors4IntegrationEvent.forEach {
-                it.postPersist(event)
-            }
+            integrationEventInterceptorManager.orderedEventInterceptors4IntegrationEvent.forEach { it.postPersist(event) }
             persistedEvents.add(event)
         }
-        val integrationEventAttachedTransactionCommittedEvent =
-            IntegrationEventAttachedTransactionCommittedEvent(
-                this, persistedEvents
-            )
-        applicationEventPublisher.publishEvent(integrationEventAttachedTransactionCommittedEvent)
+
+        applicationEventPublisher.publishEvent(
+            IntegrationEventAttachedTransactionCommittedEvent(this, persistedEvents)
+        )
     }
 
     override fun <EVENT : Any> publish(eventPayload: EVENT, schedule: LocalDateTime, delay: Duration) {
-        val persistedEvents = ArrayList<EventRecord>(1)
-        val event = eventRecordRepository.create()
-        event.init(
-            eventPayload,
-            this.svcName,
-            schedule,
-            Duration.ofMinutes(DEFAULT_EVENT_EXPIRE_MINUTES),
-            DEFAULT_EVENT_RETRY_TIMES
-        )
-        event.markPersist(true)
-        integrationEventInterceptorManager.orderedEventInterceptors4IntegrationEvent.forEach {
-            it.prePersist(event)
-        }
-        eventRecordRepository.save(event)
-        integrationEventInterceptorManager.orderedEventInterceptors4IntegrationEvent.forEach {
-            it.postPersist(event)
-        }
-        persistedEvents.add(event)
-        val integrationEventAttachedTransactionCommittedEvent =
-            IntegrationEventAttachedTransactionCommittedEvent(
-                this, persistedEvents
+        val event = eventRecordRepository.create().apply {
+            init(
+                eventPayload,
+                svcName,
+                schedule,
+                Duration.ofMinutes(DEFAULT_EVENT_EXPIRE_MINUTES),
+                DEFAULT_EVENT_RETRY_TIMES
             )
-        applicationEventPublisher.publishEvent(integrationEventAttachedTransactionCommittedEvent)
+            markPersist(true)
+        }
+
+        integrationEventInterceptorManager.orderedEventInterceptors4IntegrationEvent.forEach { it.prePersist(event) }
+        eventRecordRepository.save(event)
+        integrationEventInterceptorManager.orderedEventInterceptors4IntegrationEvent.forEach { it.postPersist(event) }
+
+        applicationEventPublisher.publishEvent(
+            IntegrationEventAttachedTransactionCommittedEvent(this, listOf(event))
+        )
     }
 
     @TransactionalEventListener(
         fallbackExecution = true,
         classes = [IntegrationEventAttachedTransactionCommittedEvent::class]
     )
-    fun onTransactionCommitted(integrationEventAttachedTransactionCommittedEvent: IntegrationEventAttachedTransactionCommittedEvent) {
-        val events = integrationEventAttachedTransactionCommittedEvent.events
-        publish(events)
+    fun onTransactionCommitted(event: IntegrationEventAttachedTransactionCommittedEvent) {
+        event.events.takeIf { it.isNotEmpty() }?.forEach(eventPublisher::publish)
     }
-
-    private fun publish(events: List<EventRecord>) {
-        if (events.isNotEmpty()) {
-            events.forEach(eventPublisher::publish)
-        }
-    }
-
 
     fun reset() {
         TL_EVENT_PAYLOADS.remove()
@@ -173,34 +170,35 @@ open class DefaultIntegrationEventSupervisor(
 
     /**
      * 弹出事件列表
+     * 获取并清除当前线程的事件列表
      *
      * @return 事件列表
      */
     protected fun popEvents(): Set<Any> {
-        val eventPayloads = TL_EVENT_PAYLOADS.get()
-        TL_EVENT_PAYLOADS.remove()
-        return eventPayloads
-            ?: EMPTY_EVENT_PAYLOADS
+        return TL_EVENT_PAYLOADS.get()?.also { TL_EVENT_PAYLOADS.remove() } ?: EMPTY_EVENT_PAYLOADS
     }
-
 
     /**
      * 记录事件发送时间
      *
-     * @param eventPayload
-     * @param schedule
+     * @param eventPayload 事件负载
+     * @param schedule 发送时间
      */
     protected fun putDeliverTime(eventPayload: Any, schedule: LocalDateTime) {
-        val eventScheduleMap =
-            TL_EVENT_SCHEDULE_MAP.get() ?: HashMap<Any, LocalDateTime>().also { TL_EVENT_SCHEDULE_MAP.set(it) }
+        val eventScheduleMap = TL_EVENT_SCHEDULE_MAP.get() ?: HashMap<Any, LocalDateTime>().also {
+            TL_EVENT_SCHEDULE_MAP.set(it)
+        }
         eventScheduleMap[eventPayload] = schedule
     }
 
-    fun getDeliverTime(eventPayload: Any): LocalDateTime {
-        val eventScheduleMap = TL_EVENT_SCHEDULE_MAP.get() ?: HashMap()
-        return if (eventScheduleMap.containsKey(eventPayload)) eventScheduleMap[eventPayload]!!
-        else LocalDateTime.now()
-    }
+    /**
+     * 获取事件发送时间
+     *
+     * @param eventPayload 事件负载
+     * @return 发送时间，如果未设置则返回当前时间
+     */
+    fun getDeliverTime(eventPayload: Any): LocalDateTime =
+        TL_EVENT_SCHEDULE_MAP.get()?.get(eventPayload) ?: LocalDateTime.now()
 
     companion object {
         private val TL_EVENT_PAYLOADS = ThreadLocal<MutableSet<Any>>()
