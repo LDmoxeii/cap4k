be28457247f1887638054f9c1f233aa2c18dc73f
LD_moxeii <15976781430@163.com>
2025-06-01 13:11:49 +0800

    add build configuration for ddd-core and ddd-distributed-locker-jdbc modules

---
+++ ddd-distributed-locker-jdbc/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    // Apply the shared build logic from a convention plugin.
+    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
+    id("buildsrc.convention.kotlin-jvm")
+
+    // Apply the Application plugin to add support for building an executable JVM application.
+    application
+}
+
+dependencies {
+    api(platform(libs.springBootDependencies))
+    compileOnly(libs.springJdbc)
+
+    implementation(libs.kotlinLogging)
+}
