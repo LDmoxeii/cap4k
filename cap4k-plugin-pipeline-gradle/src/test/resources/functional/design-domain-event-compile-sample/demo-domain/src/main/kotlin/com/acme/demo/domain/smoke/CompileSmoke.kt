5177e97d9a3bbf5c83573090e56eb1a9eefd7c2f
zhenyu.jiang <jiangzhenyu@keyfil.com>
2026-04-25 18:18:23 +0800

    test: align compile fixtures with domain event layout

--- cap4k-plugin-pipeline-gradle/src/test/resources/functional/design-domain-event-compile-sample/demo-domain/src/main/kotlin/com/acme/demo/domain/smoke/CompileSmoke.kt
+++ cap4k-plugin-pipeline-gradle/src/test/resources/functional/design-domain-event-compile-sample/demo-domain/src/main/kotlin/com/acme/demo/domain/smoke/CompileSmoke.kt
@@ -1,6 +1,6 @@
 package com.acme.demo.domain.smoke
 
-import com.acme.demo.domain.order.events.OrderCreatedDomainEvent
+import com.acme.demo.domain.aggregates.order.events.OrderCreatedDomainEvent
 
 object CompileSmoke {
     val eventType = OrderCreatedDomainEvent::class
