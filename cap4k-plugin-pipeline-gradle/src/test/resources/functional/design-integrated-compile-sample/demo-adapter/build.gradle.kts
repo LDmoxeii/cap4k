4667598734a7ae9f124cb07e5f7b454ab3e83cbe
zhenyu.jiang <15976781430@163.com>
2026-05-14 21:24:05 +0800

    build: rename published gradle plugin ids

--- cap4k-plugin-pipeline-gradle/src/test/resources/functional/design-integrated-compile-sample/demo-adapter/build.gradle.kts
+++ cap4k-plugin-pipeline-gradle/src/test/resources/functional/design-integrated-compile-sample/demo-adapter/build.gradle.kts
@@ -9,7 +9,7 @@ kotlin {
 dependencies {
     implementation(project(":demo-domain"))
     implementation(project(":demo-application"))
-    implementation("io.github.ldmoxeii:ddd-core:0.5.0-dev")
+    implementation("io.github.ldmoxeii:ddd-core:0.6.0-dev")
     implementation("org.springframework:spring-context")
 }
 
